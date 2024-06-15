8e65fca1e642acd5da4784d784d871d79d2bf93d
1103409364 <1103409364@qq.com>
2024-06-15 16:55:03 +0800

    eslint 调整
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- prettier.config.js
+++ prettier.config.js
@@ -3,12 +3,14 @@
  */
 
 module.exports = {
-  tabWidth: 2,
+  printWidth: 120, // 最大行宽
+  tabWidth: 2, // tab 距离
   useTabs: false,
   singleQuote: true,
   quoteProps: 'as-needed',
   jsxSingleQuote: false,
-  trailingComma: 'es5',
+  trailingComma: 'es5', // 多行时，尽可能打印尾随逗号
+  semi: true, // 行尾自动加分号
   bracketSpacing: true,
   jsxBracketSameLine: false,
   arrowParens: 'always',
