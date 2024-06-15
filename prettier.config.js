6607771c0fdbda27705e1efa91610f3c5fe82702
wcs <1103409364@qq.com>
2024-06-15 12:02:26 +0800

    fix: eslint prettier 配置
    Signed-off-by: wcs <1103409364@qq.com>

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @description 代码规范
+ */
+
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: true,
+  endOfLine: 'lf',
+};
