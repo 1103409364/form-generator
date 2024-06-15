a62debf87b37ca0af6b4c046ef44d7b4d0943bc1
1103409364 <1103409364@qq.com>
2024-06-15 17:47:45 +0800

    fix: 修复 prettier eslint 配置，升级 vue-cli5
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,47 +5,69 @@ module.exports = {
     es6: true,
     browser: true,
   },
-  extends: [
-    'plugin:vue/recommended',
-    'eslint:recommended',
-    // '@vue/eslint-config-prettier'
-  ],
-  rules: {
-    // 'prettier/prettier': ['error', { semi: true }],
-    'linebreak-style': 0,
-    'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/no-unused-components': 0,
-    'no-unused-vars': 0,
-    'import/order': 0,
-    'import/extensions': 0,
-    'import/no-unresolved': 0,
-    'comma-dangle': [2, 'never'],
-    // semi: [2, 'never'],
-    'no-unused-expressions': 0,
-    'no-plusplus': 0,
-    'import/prefer-default-export': 0,
-    'no-use-before-define': 0,
-    'no-param-reassign': 0,
-    'no-underscore-dangle': 0,
-    'arrow-parens': [2, 'as-needed'],
-    'vue/max-attributes-per-line': 0,
-    'max-len': [
-      1,
-      {
-        code: 120,
-      },
-    ],
-    'no-eval': 0,
-    'no-multi-assign': 0,
-    'prefer-rest-params': 0,
-    'vue/require-prop-types': 0,
-    'no-restricted-globals': 0,
-  },
+  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/eslint-config-prettier'],
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: '6',
+    sourceType: 'module',
+    parser: '@babel/eslint-parser', // 使用babel解析器才能支持 jsx
+    // "requireConfigFile": false,
+    ecmaFeatures: {
+      // 支持装饰器
+      // "legacyDecorators": true,
+      jsx: true, // 支持jsx
+    },
   },
-  globals: {
-    location: false,
+  plugins: ['vue'],
+  rules: {
+    // camelcase: 2, // 驼峰命名
+    // 'no-console': 1,
+    'no-debugger': 1,
+    'prettier/prettier': ['error', { semi: true }],
+    'vue/no-v-html': 0,
+    'vue/no-mutating-props': 0, // props 不可修改
+    'vue/multi-word-component-names': 0,
+    'vue/require-prop-types': 0, // props 类型必填
+    'vue/component-name-in-template-casing': [
+      'error',
+      'kebab-case',
+      {
+        registeredComponentsOnly: false,
+        ignores: [],
+      },
+    ], // dom 模板中组件名 kebab-case
+    // "vue/name-property-casing": ["error", "PascalCase"], //组件名PascalCase
+    // "vue/match-component-file-name": [
+    //   "error",
+    //   { "extensions": ["vue"], "shouldMatchCase": true }
+    // ], //要求组件name属性匹配其文件名
+    'vue/prop-name-casing': ['error', 'camelCase'], //组件内 props camelCase
+    'vue/this-in-template': ['error', 'never'], //不要在模版中使用this
+    // 'vue/custom-event-name-casing': ['error', 'kebab-case'], //自定义事件名称始终使用kebab-case(短横线命名)
+    'vue/attribute-hyphenation': ['error', 'always', { ignore: [] }], //标签属性名称始终使用kebab-case(短横线命名)
+    // "vue/html-self-closing": 0, // 标签自动闭合
+    'vue/attributes-order': [
+      'error',
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'UNIQUE',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+      },
+    ], //标签属性、指令排序
   },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: { jest: true },
+    },
+  ],
 };
--- vue.config.js
+++ vue.config.js
@@ -1,57 +1,135 @@
-const path = require('path')
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
-const minify = process.env.NODE_ENV === 'development' ? false : {
-  collapseWhitespace: true,
-  removeComments: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  useShortDoctype: true,
-  minifyCSS: true,
-  minifyJS: true
-}
+const minify =
+  process.env.NODE_ENV === 'development'
+    ? false
+    : {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      };
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/form-generator/'
-    : '/',
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/form-generator/' : '/',
   pages: {
     index: {
       entry: 'src/views/index/main.js',
       template: 'public/index.html',
       filename: 'index.html',
       chunks: ['chunk-vendors', 'chunk-common', 'index'],
-      minify
+      minify,
     },
     preview: {
       entry: 'src/views/preview/main.js',
       template: 'public/preview.html',
       filename: 'preview.html',
       chunks: ['chunk-vendors', 'chunk-common', 'preview'],
-      minify
-    }
+      minify,
+    },
   },
   devServer: {
-    overlay: false
+    hot: true,
+    // port: devPort,
+    open: false,
+    client: {
+      logging: 'info',
+      overlay: true,
+      progress: true,
+    },
+    // proxy: {
+    //   '/api/mock': {
+    //     target: process.env.VUE_APP_PROXY_MOCK,
+    //     changOrigin: true,
+    //     pathRewrite: { '^/api/mock': '' },
+    //   },
+    //   '/api': {
+    //     target: process.env.VUE_APP_PROXY,
+    //     changOrigin: true,
+    //     pathRewrite: { '^/api': '' },
+    //   },
+    // },
   },
   productionSourceMap: false,
-  configureWebpack: {
-    externals: {
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      'element-ui': 'ELEMENT'
-    }
+  // configureWebpack: {
+  //   externals: {
+  //     vue: 'Vue',
+  //     'vue-router': 'VueRouter',
+  //     'element-ui': 'ELEMENT',
+  //   },
+  // },
+  configureWebpack(config) {
+    // config.devtool = 'source-map';
+    // const sassLoader = require.resolve('sass-loader');
+    // config.module.rules
+    //   .filter((rule) => {
+    //     return rule.test.toString().indexOf('scss') !== -1;
+    //   })
+    //   .forEach((rule) => {
+    //     rule.oneOf.forEach((oneOfRule) => {
+    //       const sassLoaderIndex = oneOfRule.use.findIndex((item) => item.loader === sassLoader);
+    //       oneOfRule.use.splice(sassLoaderIndex, 0, {
+    //         loader: require.resolve('css-unicode-loader'),
+    //       });
+    //     });
+    //   });
+
+    return {
+      cache: {
+        // 将缓存类型设置为文件系统,默认是 memory。修改 .browserslistrc 后要关闭缓存重新编译
+        type: 'filesystem',
+        buildDependencies: {
+          // 更改配置文件时，重新缓存
+          config: [__filename],
+        },
+      },
+      // watchOptions: {
+      //   ignored: ['**/message', '**/node_modules'], // https://webpack.docschina.org/configuration/watch/
+      // },
+      optimization: {
+        // 值为"single"会创建一个在所有生成chunk之间共享的运行时文件
+        runtimeChunk: 'single',
+        moduleIds: 'deterministic',
+      },
+      externals: {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        'element-ui': 'ELEMENT',
+      },
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+        },
+        // fallback: {
+        // fs: false,
+        // util: false,
+        // tls: false,
+        // net: false,
+        // path: false,
+        // zlib: false,
+        // http: false,
+        // https: false,
+        // stream: false,
+        // crypto: false,
+        // path: require.resolve('path-browserify'),
+        // 'crypto-browserify': require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify
+        // },
+      },
+    };
   },
   chainWebpack(config) {
     // set svg-sprite-loader
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
+    config.module.rule('svg').exclude.add(resolve('src/icons')).end();
     config.module
       .rule('icons')
       .test(/\.svg$/)
@@ -60,8 +138,8 @@ module.exports = {
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
-        symbolId: 'icon-[name]'
+        symbolId: 'icon-[name]',
       })
-      .end()
-  }
-}
+      .end();
+  },
+});
