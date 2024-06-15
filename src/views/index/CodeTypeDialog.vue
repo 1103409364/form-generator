9772fcb2c58c3dbadbff0406a7a502b63b85e9a0
1103409364 <1103409364@qq.com>
2024-06-15 17:48:51 +0800

    style: format
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- src/views/index/CodeTypeDialog.vue
+++ src/views/index/CodeTypeDialog.vue
@@ -10,13 +10,7 @@
       @close="onClose"
     >
       <el-row :gutter="15">
-        <el-form
-          ref="elForm"
-          :model="formData"
-          :rules="rules"
-          size="medium"
-          label-width="100px"
-        >
+        <el-form ref="elForm" :model="formData" :rules="rules" size="medium" label-width="100px">
           <el-col :span="24">
             <el-form-item label="生成类型" prop="type">
               <el-radio-group v-model="formData.type">
@@ -38,73 +32,73 @@
       </el-row>
 
       <div slot="footer">
-        <el-button @click="close">
-          取消
-        </el-button>
-        <el-button type="primary" @click="handelConfirm">
-          确定
-        </el-button>
+        <el-button @click="close">取消</el-button>
+        <el-button type="primary" @click="handelConfirm">确定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 <script>
-export default {
-  inheritAttrs: false,
-  props: ['showFileName'],
-  data() {
-    return {
-      formData: {
-        fileName: undefined,
-        type: 'file'
+  export default {
+    inheritAttrs: false,
+    props: ['showFileName'],
+    data() {
+      return {
+        formData: {
+          fileName: undefined,
+          type: 'file',
+        },
+        rules: {
+          fileName: [
+            {
+              required: true,
+              message: '请输入文件名',
+              trigger: 'blur',
+            },
+          ],
+          type: [
+            {
+              required: true,
+              message: '生成类型不能为空',
+              trigger: 'change',
+            },
+          ],
+        },
+        typeOptions: [
+          {
+            label: '页面',
+            value: 'file',
+          },
+          {
+            label: '弹窗',
+            value: 'dialog',
+          },
+        ],
+      };
+    },
+    computed: {},
+    watch: {},
+    mounted() {},
+    methods: {
+      onOpen() {
+        if (this.showFileName) {
+          this.formData.fileName = `${+new Date()}.vue`;
+        }
       },
-      rules: {
-        fileName: [{
-          required: true,
-          message: '请输入文件名',
-          trigger: 'blur'
-        }],
-        type: [{
-          required: true,
-          message: '生成类型不能为空',
-          trigger: 'change'
-        }]
+      onClose() {},
+      close(e) {
+        console.log('close', e);
+        this.$emit('update:visible', false);
       },
-      typeOptions: [{
-        label: '页面',
-        value: 'file'
-      }, {
-        label: '弹窗',
-        value: 'dialog'
-      }]
-    }
-  },
-  computed: {
-  },
-  watch: {},
-  mounted() {},
-  methods: {
-    onOpen() {
-      if (this.showFileName) {
-        this.formData.fileName = `${+new Date()}.vue`
-      }
+      handelConfirm() {
+        this.$refs.elForm.validate((valid) => {
+          if (!valid) return;
+          this.$emit('confirm', { ...this.formData });
+          this.close();
+        });
+      },
     },
-    onClose() {
-    },
-    close(e) {
-      this.$emit('update:visible', false)
-    },
-    handelConfirm() {
-      this.$refs.elForm.validate(valid => {
-        if (!valid) return
-        this.$emit('confirm', { ...this.formData })
-        this.close()
-      })
-    }
-  }
-}
+  };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/index/JsonDrawer.vue
+++ src/views/index/JsonDrawer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-drawer v-bind="$attrs" v-on="$listeners" @opened="onOpen" @close="onClose">
-      <div class="action-bar" :style="{'text-align': 'left'}">
+      <div class="action-bar" :style="{ 'text-align': 'left' }">
         <span class="bar-btn" @click="refresh">
           <i class="el-icon-refresh" />
           刷新
@@ -25,120 +25,121 @@
 </template>
 
 <script>
-import { beautifierConf } from '@/utils/index'
-import ClipboardJS from 'clipboard'
-import { saveAs } from 'file-saver'
-import loadMonaco from '@/utils/loadMonaco'
-import loadBeautifier from '@/utils/loadBeautifier'
+  import { beautifierConf } from '@/utils/index';
+  import ClipboardJS from 'clipboard';
+  import { saveAs } from 'file-saver';
+  import loadMonaco from '@/utils/loadMonaco';
+  import loadBeautifier from '@/utils/loadBeautifier';
 
-let beautifier
-let monaco
+  let beautifier;
+  let monaco;
 
-export default {
-  components: {},
-  props: {
-    jsonStr: {
-      type: String,
-      required: true
-    }
-  },
-  data() {
-    return {}
-  },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {
-    window.addEventListener('keydown', this.preventDefaultSave)
-    const clipboard = new ClipboardJS('.copy-json-btn', {
-      text: trigger => {
-        this.$notify({
-          title: '成功',
-          message: '代码已复制到剪切板，可粘贴。',
-          type: 'success'
-        })
-        return this.beautifierJson
-      }
-    })
-    clipboard.on('error', e => {
-      this.$message.error('代码复制失败')
-    })
-  },
-  beforeDestroy() {
-    window.removeEventListener('keydown', this.preventDefaultSave)
-  },
-  methods: {
-    preventDefaultSave(e) {
-      if (e.key === 's' && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault()
-      }
+  export default {
+    components: {},
+    props: {
+      jsonStr: {
+        type: String,
+        required: true,
+      },
     },
-    onOpen() {
-      loadBeautifier(btf => {
-        beautifier = btf
-        this.beautifierJson = beautifier.js(this.jsonStr, beautifierConf.js)
+    data() {
+      return {};
+    },
+    computed: {},
+    watch: {},
+    created() {},
+    mounted() {
+      window.addEventListener('keydown', this.preventDefaultSave);
+      const clipboard = new ClipboardJS('.copy-json-btn', {
+        text: (trigger) => {
+          console.log(trigger);
+          this.$notify({
+            title: '成功',
+            message: '代码已复制到剪切板，可粘贴。',
+            type: 'success',
+          });
+          return this.beautifierJson;
+        },
+      });
+      clipboard.on('error', (e) => {
+        this.$message.error('代码复制失败', e);
+      });
+    },
+    beforeDestroy() {
+      window.removeEventListener('keydown', this.preventDefaultSave);
+    },
+    methods: {
+      preventDefaultSave(e) {
+        if (e.key === 's' && (e.metaKey || e.ctrlKey)) {
+          e.preventDefault();
+        }
+      },
+      onOpen() {
+        loadBeautifier((btf) => {
+          beautifier = btf;
+          this.beautifierJson = beautifier.js(this.jsonStr, beautifierConf.js);
 
-        loadMonaco(val => {
-          monaco = val
-          this.setEditorValue('editorJson', this.beautifierJson)
-        })
-      })
+          loadMonaco((val) => {
+            monaco = val;
+            this.setEditorValue('editorJson', this.beautifierJson);
+          });
+        });
+      },
+      onClose() {},
+      setEditorValue(id, codeStr) {
+        if (this.jsonEditor) {
+          this.jsonEditor.setValue(codeStr);
+        } else {
+          this.jsonEditor = monaco.editor.create(document.getElementById(id), {
+            value: codeStr,
+            theme: 'vs-dark',
+            language: 'json',
+            automaticLayout: true,
+          });
+          // ctrl + s 刷新
+          this.jsonEditor.onKeyDown((e) => {
+            if (e.keyCode === 49 && (e.metaKey || e.ctrlKey)) {
+              this.refresh();
+            }
+          });
+        }
+      },
+      exportJsonFile() {
+        this.$prompt('文件名:', '导出文件', {
+          inputValue: `${+new Date()}.json`,
+          closeOnClickModal: false,
+          inputPlaceholder: '请输入文件名',
+        }).then(({ value }) => {
+          if (!value) value = `${+new Date()}.json`;
+          const codeStr = this.jsonEditor.getValue();
+          const blob = new Blob([codeStr], { type: 'text/plain;charset=utf-8' });
+          saveAs(blob, value);
+        });
+      },
+      refresh() {
+        try {
+          this.$emit('refresh', JSON.parse(this.jsonEditor.getValue()));
+        } catch (error) {
+          this.$notify({
+            title: '错误',
+            message: 'JSON格式错误，请检查',
+            type: 'error',
+          });
+        }
+      },
     },
-    onClose() {},
-    setEditorValue(id, codeStr) {
-      if (this.jsonEditor) {
-        this.jsonEditor.setValue(codeStr)
-      } else {
-        this.jsonEditor = monaco.editor.create(document.getElementById(id), {
-          value: codeStr,
-          theme: 'vs-dark',
-          language: 'json',
-          automaticLayout: true
-        })
-        // ctrl + s 刷新
-        this.jsonEditor.onKeyDown(e => {
-          if (e.keyCode === 49 && (e.metaKey || e.ctrlKey)) {
-            this.refresh()
-          }
-        })
-      }
-    },
-    exportJsonFile() {
-      this.$prompt('文件名:', '导出文件', {
-        inputValue: `${+new Date()}.json`,
-        closeOnClickModal: false,
-        inputPlaceholder: '请输入文件名'
-      }).then(({ value }) => {
-        if (!value) value = `${+new Date()}.json`
-        const codeStr = this.jsonEditor.getValue()
-        const blob = new Blob([codeStr], { type: 'text/plain;charset=utf-8' })
-        saveAs(blob, value)
-      })
-    },
-    refresh() {
-      try {
-        this.$emit('refresh', JSON.parse(this.jsonEditor.getValue()))
-      } catch (error) {
-        this.$notify({
-          title: '错误',
-          message: 'JSON格式错误，请检查',
-          type: 'error'
-        })
-      }
-    }
-  }
-}
+  };
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/mixin.scss';
+  @import '@/styles/mixin.scss';
 
-::v-deep .el-drawer__header {
-  display: none;
-}
-@include action-bar;
+  ::v-deep .el-drawer__header {
+    display: none;
+  }
+  @include action-bar;
 
-.json-editor{
-  height: calc(100vh - 33px);
-}
+  .json-editor {
+    height: calc(100vh - 33px);
+  }
 </style>
